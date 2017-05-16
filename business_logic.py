dbc1b8c9159f761a504db17f78b9cf55537d5b24
Balogh Máté <bmate11@bmate11>
2017-05-16 11:39:27 +0200

    2nd question was implemented, console_app merged into main menu func

--- business_logic.py
+++ business_logic.py
@@ -6,19 +6,17 @@ def main_menu():
     print("Main menu:\nPlease select a question.\n")
     for menu in range(1, 8):
         print("{0}. Question {0}".format(menu))
+
     selected_q = int(input("\nSelected question: "))
-    return selected_q
-
-
-def console_app():
-
-    selected_q = main_menu()
     fetched_data = data_handler(selected_q)
     return fetched_data
 
 
 def print_out_data():
 
-    result = console_app()
+    result = main_menu()
     for row in result:
-        print(row[0], row[1])
+        if len(row) == 2:
+            print(row[0], row[1])
+        else:
+            print(row[0])
--- data_manager.py
+++ data_manager.py
@@ -3,7 +3,8 @@ import psycopg2
 
 def data_handler(query):
 
-    options = {1: """SELECT first_name, last_name FROM mentors"""}
+    options = {1: """SELECT first_name, last_name FROM mentors""",
+               2: """SELECT nick_name FROM mentors WHERE city='Miskolc'"""}
 
     try:
         connection_str = "dbname='bmate11' user='bmate11' host='localhost' password='3dc41885'"
