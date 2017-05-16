c4b9ba38739d26fa545f9b592e549c9c60453a27
Balogh Máté <bmate11@bmate11>
2017-05-16 13:56:05 +0200

    loop was created for the menu, sys module imported for the exit

--- business_logic.py
+++ business_logic.py
@@ -1,13 +1,18 @@
 from data_manager import data_handler
+import sys
 
 
 def main_menu():
 
-    print("Main menu:\nPlease select a question.\n")
+    print("\nMain menu:\nPlease select a question.\n")
     for menu in range(1, 8):
         print("{0}. Question {0}".format(menu))
+    print("8. Press 0 to exit.")
 
-    selected_q = int(input("\nSelected question: "))
+    selected_q = int(input("\nSelected question: \n"))
+    if not selected_q:
+        sys.exit()
+
     fetched_data = data_handler(selected_q)
     return fetched_data
 
@@ -16,7 +21,9 @@ def print_out_data():
 
     result = main_menu()
     for row in result:
-        if len(row) == 2:
+        if len(row) == 1:
+            print(row[0])
+        elif len(row) == 2:
             print(row[0], row[1])
         else:
-            print(row[0])
+            print(row[0], row[1], row[2], row[3], row[4], row[5])
--- main.py
+++ main.py
@@ -2,7 +2,8 @@ from business_logic import print_out_data
 
 
 def main():
-    print_out_data()
+    while True:
+        print_out_data()
 
 
 if __name__ == "__main__":
